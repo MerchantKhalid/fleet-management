0c1277c6168738a21eb0ec5eb68f7714a0632930
mohammad khalid hasan <rabbikhalidhasan@gmail.com>
2026-09-02 11:55:53 +0200

    login disabled

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,10 +1,22 @@
 // Blocks access to a route unless the user is logged in.
 // Any route mounted after this middleware in app.js will redirect to /login if not authenticated.
+// function requireAuth(req, res, next) {
+  // if (req.session && req.session.loggedIn) {
+  //   return next();
+ //  }
+  // return res.redirect('/login');
+// }
+
+// module.exports = { requireAuth };
+
+
+
+
 function requireAuth(req, res, next) {
+  return next(); // <-- login check bypassed; remove this line to re-enable auth
+
   if (req.session && req.session.loggedIn) {
     return next();
   }
   return res.redirect('/login');
 }
-
-module.exports = { requireAuth };
